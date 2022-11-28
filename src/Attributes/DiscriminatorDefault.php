fc22bd48f2eb37f9e14bb44331a3263fbe94d473
Maxim Ageev <maxim.ageev@paxful.com>
2022-11-28 20:33:15 +0400

    code coverage

--- src/Attributes/DiscriminatorDefault.php
+++ src/Attributes/DiscriminatorDefault.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace Legion112\SerializerDiscriminatorDefault\Attributes;
 
 #[\Attribute(\Attribute::TARGET_CLASS)]
-/** @codeCoverageIgnore  */
 class DiscriminatorDefault
 {
     public function __construct(public readonly string $class)
