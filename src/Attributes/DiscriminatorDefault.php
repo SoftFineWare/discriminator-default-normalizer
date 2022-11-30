99724af6b73cd33666b61618fe1142f8f5d25301
Maxim Ageev <maxim.ageev@paxful.com>
2022-11-30 19:41:39 +0400

    Make attribute final

--- src/Attributes/DiscriminatorDefault.php
+++ src/Attributes/DiscriminatorDefault.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace Legion112\SerializerDiscriminatorDefault\Attributes;
 
 #[\Attribute(\Attribute::TARGET_CLASS)]
-class DiscriminatorDefault
+final class DiscriminatorDefault
 {
     public function __construct(public readonly string $class)
     {
